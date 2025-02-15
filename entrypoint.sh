8d4d521eb29bda8bd178c56af71c1a7ac8007e0e
tushar1977 <tushar.197712@gmail.com>
2025-02-15 23:32:56 +0530

    fixed some deployment bugs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Read environment variables
-export SECRET="${SECRET}"
-export PORT="${PORT}"
-export DB_USERNAME="${DB_USERNAME}"
-export DB_PASSWORD="${DB_PASSWORD}"
-export DB_HOSTNAME="${DB_HOSTNAME}"
-export DB_NAME="${DB_NAME}"
-export DB_PORT="${DB_PORT}"
+export SECRET=$(cat /run/secrets/SECRET)
+export PORT=$(cat /run/secrets/PORT)
+export DB_USERNAME=$(cat /run/secrets/DB_USERNAME)
+export DB_PASSWORD=$(cat /run/secrets/DB_PASSWORD)
+export DB_HOSTNAME=$(cat /run/secrets/DB_HOSTNAME)
+export DB_NAME=$(cat /run/secrets/DB_NAME)
+export DB_PORT=$(cat /run/secrets/DB_PORT)
 
-# Start the application
-exec gunicorn --bind "0.0.0.0:$PORT" "run:app"
+# Start the application with Gunicorn
+exec python3 run.py
