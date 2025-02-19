6b2cebd00689a39d5faba4bfb59e9ad7eb3eed85
tushar1977 <tushar.197712@gmail.com>
2025-02-19 12:00:39 +0530

    edited for kubernates deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Read environment variables
-export SECRET=$(cat /run/secrets/SECRET)
-export PORT=$(cat /run/secrets/PORT)
-export DB_USERNAME=$(cat /run/secrets/DB_USERNAME)
-export DB_PASSWORD=$(cat /run/secrets/DB_PASSWORD)
-export DB_HOSTNAME=$(cat /run/secrets/DB_HOSTNAME)
-export DB_NAME=$(cat /run/secrets/DB_NAME)
-export DB_PORT=$(cat /run/secrets/DB_PORT)
-
-# Start the application with Gunicorn
-exec gunicorn --certfile=cert.pem --keyfile=key.pem \
+exec gunicorn --certfile=/app/tls/cert.pem --keyfile=/app/tls/key.pem \
   --worker-class eventlet -w 1 \
-  -b 0.0.0.0:${PORT} run:app
+  -b 0.0.0.0:$PORT run:app
